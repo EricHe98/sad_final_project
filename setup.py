67433c0a091c6960c34a1fe29747ca120bd09416
Eric-He-98 <eric@rocketmiles.com>
2020-10-21 21:43:19 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementing DL models for ranking, with evaluation benchmarks',
+    author='ajea',
+    license='MIT',
+)
