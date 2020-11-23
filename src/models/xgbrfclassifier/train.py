92fa86bba95fbad3eae90305ced5f5fbdf5e6e94
Eric-He-98 <eric@rocketmiles.com>
2020-11-23 17:17:08 -0500

    add xgb random forest baseline

--- src/models/xgbrfclassifier/train.py
+++ src/models/xgbrfclassifier/train.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 
 import xgboost as xgb
-from xgboost import XGBRanker, XGBRFClassifier
+from xgboost import XGBRanker, XGBClassifier, XGBRFClassifier
 
 from src.modules.modules_pandas import read_parquet, feature_label_split
 
@@ -18,17 +18,18 @@ import mlflow
 import argparse 
 
 parser = argparse.ArgumentParser(description='Use LambdaMART example model to predict on validation set.')
-parser.add_argument('run_type', choices=['small_100', 'small_all', 'full'],
-    help='type of run (small_100, small_all, all) to be included in MLFlow name')
-parser.add_argument('data_path', type=str, 
-    help='Path to train, val, test sets')
+parser.add_argument('dataset', choices=['small_100', 'small_all', 'full'],
+    help='which dataset to train the model (small_100, small_all, all) to be included in MLFlow name')
+parser.add_argument('--split', choices=['train', 'val', 'test'], default='train',
+    help='which split of the dataset to train on (train, val, test)')
 
 args = parser.parse_args()
+data_path = 'data/raw'
 
 features_path = 'src/data/schemas/output_data_schemas.json'
 
 def __main__():
-    data = read_parquet(os.path.join(args.data_path, 'train'))
+    data = read_parquet(os.path.join(data_path, args.dataset, args.split))
 
     with open(features_path, 'r') as features:
         model_feature_schemas = json.load(features)
@@ -38,16 +39,17 @@ def __main__():
     X = X.astype('float')
     y = np.where(y >= 1, 1, y)
 
-    with mlflow.start_run(run_name='{}_lambdamart'.format(args.run_type)):
+    with mlflow.start_run(run_name='xgbrfclassifier'):
         run_id = mlflow.active_run().info.run_id
         print('MLFlow Run ID is: {}'.format(run_id))
-        mlflow.log_param('run_type', args.run_type)
+        mlflow.log_param('dataset', args.dataset)
+        mlflow.log_param('train_split', args.split)
         mlflow.log_param('model_name', 'XGBRFClassifier')
         mlflow.log_param('run_id', run_id)
         # n_jobs=-1 is supposed to mean use all cores available
         # however n_jobs=-1 seems to only be parallelized on latest xgboost version 1.3.0rc1
         # make sure to check, otherwise manually set n_jobs to number of cores available
-        model = XGBRFClassifier(verbosity=2, n_jobs=8, n_estimators=10)
+        model = XGBRFClassifier(verbosity=2, n_jobs=-1, n_estimators=10)
         mlflow.log_params(model.get_params())
         time_start = dt.datetime.now()
         model.fit(X, y)
@@ -57,7 +59,7 @@ def __main__():
         mlflow.log_metric('training_time', train_time)
         print('Model trained in {}'.format(train_time))
 
-        mlflow.xgboost.log_model(model, 'xgbrfclassifier.json')
+        mlflow.xgboost.log_model(model, 'model.json')
     
 if __name__ == '__main__':
     __main__()
