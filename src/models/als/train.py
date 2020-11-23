82c5e955875ddf13cabe9438326ec311d234c675
Eric-He-98 <eric@rocketmiles.com>
2020-11-23 03:03:42 -0500

    add baselines

---
+++ src/models/als/train.py
@@ -0,0 +1,96 @@
+# findspark finds the Spark home
+# since Spark cant seem to do it itself
+# needs to be run before importing Spark
+import findspark
+findspark.init()
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.ml.recommendation import ALS
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.mllib.evaluation import RankingMetrics, RegressionMetrics
+from pyspark.sql.window import Window
+from pyspark.sql.functions import col, row_number, desc, collect_list
+from pyspark.sql.types import IntegerType
+import os
+import json
+import time
+import datetime as dt
+import mlflow
+import argparse
+
+# You can type `python3 SCRIPT_NAME --help` and argparse will auto-generate 
+# documentation of what arguments the script accepts
+parser = argparse.ArgumentParser(description='Train ALS model.')
+parser.add_argument('run_type', choices=['small_100', 'small_all', 'full'],
+    help='type of run (small_100, small_all, all) to be included in MLFlow name')
+parser.add_argument('data_path', type=str, 
+    help='Path to train, val, test sets')
+
+args = parser.parse_args()
+
+def __main__():
+    sc = SparkContext('local[*]')
+    spark = SparkSession(sc)
+    data = spark.read.parquet(os.path.join(args.data_path, 'train'))\
+        .select('user_id', 'hotel_id', 'label')\
+        .where(col('user_id').isNotNull())\
+        .where(col('label') > 0)
+
+    # ALS cannot handle bigint, need to feed in int
+    data = data.withColumn('user_id_int', (data['user_id'] - 10000000000).cast(IntegerType()))
+
+    RANK = 32
+    REG = 0.1
+    NONNEGATIVE = False
+    COLDSTART = 'drop'
+
+    # using `with` scopes the MLFlow run to start and end in this block
+    # you can alternatively just use `mlflow.start_run(blah)` to start
+    # and `mlflow.end_run()` to end
+    # Every model train should have its own MLFlow run
+    with mlflow.start_run(run_name='{}_als'.format(args.run_type)):
+        # MLFlow assigns a run_id to every model train
+        # We need to hang onto it for the prediction and evaluation steps
+        run_id = mlflow.active_run().info.run_id
+        print('MLFlow Run ID is: {}'.format(run_id))
+        
+        # MLFlow is used simply because it gives us a nice UI to log params and metrics
+        # I would like us to log at least three things for training: run_type, model_name,
+        # and training_time. You have wide latitude to choose model_name; try to keep
+        # model_name the same for models with the same hyperparameters.
+        mlflow.log_param('run_type', args.run_type)
+        mlflow.log_param('model_name', 'ALS')
+        als = ALS()\
+            .setUserCol('user_id_int')\
+            .setRatingCol('label')\
+            .setItemCol('hotel_id')\
+            .setRank(RANK)\
+            .setRegParam(REG)\
+            .setNonnegative(NONNEGATIVE)\
+            .setColdStartStrategy(COLDSTART)
+        # If it can feasibly be used in hyperparameter optimization,
+        # it should be logged to MLFlow.
+        mlflow.log_param('cold_start', COLDSTART)
+        mlflow.log_param('rank', RANK)
+        mlflow.log_param('reg', REG)
+        mlflow.log_param('nonnegative', NONNEGATIVE)
+
+        # I calculate train time this way, probably best to keep
+        # to this standard for consistency
+        time_start = dt.datetime.now()
+        model = als.fit(data)
+        time_end = dt.datetime.now()
+
+        train_time = (time_end - time_start).total_seconds()
+        mlflow.log_metric('training_time', train_time)
+        print('Model trained in {}'.format(train_time))
+        # writes model file to MLFlow directory 
+        # under {run_id}/artifacts/als
+        # in the MLFlow model format
+        # can be loaded in using mlflow.spark.load_model
+        # see https://www.mlflow.org/docs/latest/python_api/mlflow.pyfunc.html#mlflow.pyfunc.load_model
+        # for loading options; I use the "runs" method to load in the predict script
+        mlflow.spark.log_model(model, 'als') 
+
+if __name__ == '__main__':
+    __main__()
---
+++ src/models/xgbrfclassifier/train.py
@@ -0,0 +1,63 @@
+import os
+import json
+import pickle
+import sys
+import traceback
+import datetime as dt
+import click
+
+import numpy as np 
+import pandas as pd
+
+import xgboost as xgb
+from xgboost import XGBRanker, XGBRFClassifier
+
+from src.modules.modules_pandas import read_parquet, feature_label_split
+
+import mlflow
+import argparse 
+
+parser = argparse.ArgumentParser(description='Use LambdaMART example model to predict on validation set.')
+parser.add_argument('run_type', choices=['small_100', 'small_all', 'full'],
+    help='type of run (small_100, small_all, all) to be included in MLFlow name')
+parser.add_argument('data_path', type=str, 
+    help='Path to train, val, test sets')
+
+args = parser.parse_args()
+
+features_path = 'src/data/schemas/output_data_schemas.json'
+
+def __main__():
+    data = read_parquet(os.path.join(args.data_path, 'train'))
+
+    with open(features_path, 'r') as features:
+        model_feature_schemas = json.load(features)
+        model_features = [f['name'] for f in model_feature_schemas if f['train']]
+
+    X, y, qid = feature_label_split(data, model_features, qid='search_request_id')
+    X = X.astype('float')
+    y = np.where(y >= 1, 1, y)
+
+    with mlflow.start_run(run_name='{}_lambdamart'.format(args.run_type)):
+        run_id = mlflow.active_run().info.run_id
+        print('MLFlow Run ID is: {}'.format(run_id))
+        mlflow.log_param('run_type', args.run_type)
+        mlflow.log_param('model_name', 'XGBRFClassifier')
+        mlflow.log_param('run_id', run_id)
+        # n_jobs=-1 is supposed to mean use all cores available
+        # however n_jobs=-1 seems to only be parallelized on latest xgboost version 1.3.0rc1
+        # make sure to check, otherwise manually set n_jobs to number of cores available
+        model = XGBRFClassifier(verbosity=2, n_jobs=8, n_estimators=10)
+        mlflow.log_params(model.get_params())
+        time_start = dt.datetime.now()
+        model.fit(X, y)
+        time_end = dt.datetime.now()
+
+        train_time = (time_end - time_start).total_seconds()
+        mlflow.log_metric('training_time', train_time)
+        print('Model trained in {}'.format(train_time))
+
+        mlflow.xgboost.log_model(model, 'xgbrfclassifier.json')
+    
+if __name__ == '__main__':
+    __main__()
