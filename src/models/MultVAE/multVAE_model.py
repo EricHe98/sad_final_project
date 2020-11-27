05199bb344b9665c22e395ad402aa3df0e31f914
Alec Hon <alechon2010@gmail.com>
2020-11-26 19:07:02 -0500

    work on architecture

--- src/models/MultVAE/multVAE_model.py
+++ src/models/MultVAE/multVAE_model.py
@@ -1,7 +1,28 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as functional
+import torch.nn.functional as f
 import numpy as numpy
 
-class MultVae():
-	raise NotImplementedError
+class MultVAE_encoder(nn.module):
+	def __init(self, item_dim, latent_dim, n_hidden_layers, dropout = 0.5):
+		self.item_dim = item_dim
+		self.latent_dim = latent_dim
+		self.first_layer = nn.Linear(in_features = item_dim, out_features = 600)
+
+class MultVAE_decoder(nn.module):
+	def __init(self, item_dim, latent_dim, n_hidden_layers, dropout = 0.5):
+		self.item_dim = item_dim
+		self.latent_dim = latent_dim
+		self.first_layer = nn.Linear(in_features = item_dim, out_features = 600)
+
+
+class MultVae(nn.module):
+	def __init__(self, item_dim, latent_dim, n_hidden_layers,)
+
+
+
+
+def VAE_loss_function(x_hat, x, mu, logvar, beta = 1.00):
+	bce = f.binary_cross_entropy(x_hat, x)
+	kl_div = -0.5 * torch.mean(torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1))
+	return bce + beta * kl_div
