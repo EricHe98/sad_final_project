cbd436cffb66ceade25c977c94f636b05c7f249f
Alex Dong <alexwdong@gmail.com>
2020-12-06 03:20:17 -0500

    adding predict for multvae

--- src/models/MultVAE/predict.py
+++ src/models/MultVAE/predict.py
@@ -8,21 +8,23 @@ import datetime as dt
 import numpy as np 
 import pandas as pd
 
-from  src.data.preprocessing import get_user_entire_interaction_vec
-
 import mlflow
+import torch
+from torch.utils.data import Dataset
+from MultVAE_Dataset import BasicHotelDataset
+from scipy import sparse
 
 import argparse
 parser = argparse.ArgumentParser(description='Use MultVAE model to predict on validation set.')
 parser.add_argument('-r',
                     '--run_id',
-                    type=int,
+                    type=str,
                     required=True,
                     ) 
 parser.add_argument('-m',
                     '--multvae_model', 
                     type = str,
-		    required=True,
+                    required=True,
                     help='multVAE model. Should be a pytorch checkpoint (.pth file). Needs to be MultVAE class.',
                     )
                     
@@ -32,12 +34,20 @@ parser.add_argument('-d',
                     type = str,
                     help='dataset pkl. Should be a user_to_queries.pkl. Check preprocessing.py for info on that structure',
                     default ='/scratch/work/js11133/sad_data/processed/full/val/user_to_queries.pkl' )
-parser.add_argument('-h',
+
+parser.add_argument('-i',
                     '--hotel_hash', 
                     nargs = '?',
                     type = str,
                     help='hotel_hash.json. Check make_hashes.py for info on the hash',
-                    default ='/scratch/work/js11133/sad_data/processed/hotel_hash.json' )
+                    default ='/scratch/work/js11133/sad_data/processed/hotel_hash.json')
+
+parser.add_argument('-u',
+                    '--user_hash', 
+                    nargs = '?',
+                    type = str,
+                    help='user_hash.json. Check make_hashes.py for info on the hash',
+                    default ='/scratch/work/js11133/sad_data/processed/user_hash.json')
                     
 parser.add_argument('-o',
                     '--output_dir', 
@@ -48,67 +58,105 @@ parser.add_argument('-o',
 
 args = parser.parse_args()
 
+def get_single_query_interaction_vec(user_id_to_query_struct_dict,user_id,sr_id):
+    return user_id_to_query_struct_dict[user_id][0][sr_id]
+
+def get_user_entire_interaction_vec(user_id_to_query_struct_dict,user_id):
+    return user_id_to_query_struct_dict[user_id][1]
+
+def densify_sparse_vec(user_interaction_dict, hotel_length):
+    sparse_dok = sparse.dok_matrix((1,hotel_length),dtype=np.float32)
+    sparse_obs = sparse.dok_matrix((1,hotel_length),dtype=np.float32)
+    for j in user_interaction_dict.keys():
+        sparse_dok[0,j] = user_interaction_dict[j]
+        sparse_obs[0,j] = 1
+    return torch.tensor(sparse_dok.toarray()),torch.tensor(sparse_obs.toarray())
 
 def __main__():
-    mlflow.start_run(run_id=args.run_id)
+    print('IN MAIN')
+    #mlflow.start_run(run_id=args.run_id)
+    #Check for CUDA
+    if torch.cuda.is_available():
+
+        # Tell PyTorch to use the GPU.
+        device = torch.device("cuda")
+        print('There are %d GPU(s) available.' % torch.cuda.device_count())
+        print('We will use the GPU:', torch.cuda.get_device_name(0))
+    else:
+        print('No GPU available, using the CPU instead.')
+        device = torch.device("cpu") 
     # Load user to query_struct
     with open(args.dataset_pkl,'rb') as f:
         user_to_query_struct = pickle.load(f)
+    #Put the dataset into the dataloader
+    dataset = BasicHotelDataset(data_path = args.dataset_pkl, dict_path = args.hotel_hash)
+    
+    #dataloader = DataLoader(dataset, batch_size = 1024), hotel_length
+    #Create sr_id_to_user_id dictionary
+    sr_id_to_user_id_hashed = {}
+    for user_id_hashed in user_to_query_struct.keys():
+        sr_ids = user_to_query_struct[user_id_hashed][0].keys()
+        for sr_id in sr_ids:
+            sr_id_to_user_id_hashed[sr_id] = user_id_hashed
+    
+    
     # Load hotel_id to index dictionary
-    with open(hotel_hash_json_path, 'r') as fp:
+    with open(args.hotel_hash, 'r') as fp:
         hotel_id_indexed = json.load(fp)
-    #invert the map so we can go back to hotel_id
-    hotel_idx_to_hotel_id = {v: k for k, v in my_map.items()}
     
-    # Get all the user interaction vectors
-    all_user_ids = user_to_query_struct[1].keys()
-    X = [get_user_entire_interaction_vec(user_id) for user_id in all_user_ids]
-    print(X.shape)
+    # Load user_id to index dictionary
+    with open(args.user_hash, 'r') as fp:
+        user_id_indexed = json.load(fp)  
+        
+    #invert the maps so we can go back to hotel_id and user_id
+    user_idx_to_user_id = {v: k for k, v in user_id_indexed.items()}
+    hotel_idx_to_hotel_id = {v: k for k, v in hotel_id_indexed.items()}
     
-  
+    
+    # Get user_idx to/from user_id mappings 
+    dlkeys_to_user_id = dataset.idx_to_dataset_keys_dict
+    user_id_to_dlkeys = {v: k for k, v in dlkeys_to_user_id.items()}
     # Load our multVAE model
-    model = mlflow.pytorch.load_model('runs:/{}/model.json'.format(args.run_id))
-    
+    model = mlflow.pytorch.load_model(args.multvae_model)
+    model.to(device)
+    print('loading done')
     # generate predictions
-    y = model(X)
-    
-    #pred_df = 
-    
-    raise NotImplementedError
-    pred_array['score'] = model.predict(xgb.DMatrix(X))
+    user_id_list = []
+    df_list = []
+    for sr_id in sr_id_to_user_id_hashed.keys():
+        user_id = sr_id_to_user_id_hashed[sr_id]
+        user_id_unhashed = user_idx_to_user_id[user_id]
+        
+        # GET SINGLE QUERY, OR ENTIRE interaction?
+        user_interaction_vec = get_single_query_interaction_vec(user_to_query_struct,user_id,sr_id)
+        x, observed_vec = densify_sparse_vec(user_interaction_vec,dataset.hotel_length)
+        x = x.to(device)
+        
+        x_preds, mu, logvar = model(x)
+        
+        model.eval()
+
+        x_preds = pd.DataFrame({'score':x_preds.cpu().detach().squeeze().numpy(),
+                                'observed':observed_vec.cpu().detach().squeeze().numpy()}
+                              ) 
+        x_preds = x_preds[x_preds['observed']==1]
+        x_preds['hotel_id'] = x_preds.index.map(hotel_idx_to_hotel_id.get)
+        x_preds['sr_id'] = sr_id
+        x_preds['user_id'] = user_id_unhashed
+        
+        df_list.append(x_preds)
+        
+    print('end for loop')
+    pred_array = pd.concat(df_list)   
+    print('concat ended')
     pred_array['rank'] = pred_array\
-	    .groupby('search_request_id')\
-	    ['score']\
-	    .rank(ascending=False)
-
-	predictions_path = 'predictions/{}/{}/{}'.format(args.run_id, args.dataset, args.split)
-	if not os.path.exists(predictions_path):
-		os.makedirs(predictions_path)
-	predictions_file = os.path.join(predictions_path, 'predictions.parquet')
-
-	if not os.path.exists(predictions_path):
-	    os.mkdir(predictions_path)
-	    
-	pred_array.to_parquet(predictions_file)
-        '''
-	# log 95th percentile latencies predicting on 2, 10, 50, 100, 500 results
-	# It's pretty hard to get a consistent timing. There are a lot of confounds.
-	# My convention here is to assume you have a Pandas DataFrame of the data ready to go
-	N_RESULTS = [2, 10, 50, 100, 500]
-	N_TRIALS = 5000
-	for r in N_RESULTS:
-		times = []
-		for t in range(N_TRIALS):
-			sample = X.sample(r)
-			time_start = dt.datetime.now()
-			sample_dmat = xgb.DMatrix(sample)
-			model.predict(sample_dmat)
-			time_end = dt.datetime.now()
-			times.append((time_end - time_start).total_seconds() * 1000)
-		pctile_95 = np.percentile(times, 95)
-		print('95th percentile latency at {} results: {} ms'.format(r, pctile_95))
-		mlflow.log_metric('latency_{}_results'.format(r), pctile_95)
-         '''
+        .groupby('search_request_id')\
+        ['score']\
+        .rank(ascending=False)
+    if not os.path.exists(args.output_dir):
+        os.makedirs(predictions_path)
+    pred_array.to_parquet(os.path.join(args.output_dir, 'multVAE_predictions.parquet') )
+    
    
 if __name__ == '__main__':
 	__main__()
