4674f79cd88e270b1f227c549d58390bf0139d50
Eric-He-98 <eric@rocketmiles.com>
2020-12-09 03:06:31 -0500

    add train on full dataset

--- src/models/logistic_regression/train_reduced_features.py
+++ src/models/logistic_regression/train_reduced_features.py
@@ -30,8 +30,13 @@ with open(features_path, 'r') as features:
     model_feature_schemas = json.load(features)
     model_features = [f['name'] for f in model_feature_schemas if f['train']]
 
+model_features = ['hotel_cumulative_share', 'srq_price_zscore', 'previous_user_hotel_interaction',
+    'srq_rewards_zscore', 'travel_intent', 'srq_distance_zscore', 'user_preferred_price']
+
+id_features = ['search_request_id', 'hotel_id','user_id', 'label']
+
 def __main__():
-    data = read_parquet(os.path.join(data_path, args.dataset, args.split), columns= model_features)
+    data = read_parquet(os.path.join(data_path, args.dataset, args.split), columns= model_features + id_features)
 
     X, y, qid = feature_label_split(data, model_features, qid='search_request_id')
     X = X.astype('float').fillna(0)
