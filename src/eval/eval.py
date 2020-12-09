35da48975adfb5d552c4d4c11f5858bad4ad0cc9
Eric-He-98 <eric@rocketmiles.com>
2020-12-08 23:04:34 -0500

    add null fill

--- src/eval/eval.py
+++ src/eval/eval.py
@@ -23,7 +23,7 @@ from src.modules.modules_pandas import read_parquet, feature_label_split
 import src.modules.letor_metrics as lm
 
 import argparse
-parser = argparse.ArgumentParser(description='Use LambdaMART example model to predict on validation set.')
+parser = argparse.ArgumentParser(description='Evaluate a given set of predictions on a bunch of metrics.')
 parser.add_argument('run_id', type=str, 
     help='Run ID for saving models')
 parser.add_argument('dataset', choices=['small_100', 'small_all', 'full'],
@@ -35,7 +35,10 @@ args = parser.parse_args()
 data_path = 'data/raw'
 
 def __main__():
-	data = read_parquet(os.path.join(data_path, args.dataset, args.split))
+	data = read_parquet(os.path.join(data_path, args.dataset, args.split),
+		columns=['hotel_id', 'user_id', 
+			'search_request_id', 'hotel_cumulative_share',
+			'display_rank', 'price_rank', 'label'])
 
 	mlflow.start_run(args.run_id)
 
@@ -49,6 +52,16 @@ def __main__():
 	joined = data.set_index(join_keys)\
 	    .join(pred.set_index(join_keys))\
 	    .reset_index()
+
+	joined['null_pred'] = joined['score'].isnull().astype('int')
+	mlflow.log_metric('null_prediction_rate', joined['null_pred'].mean())
+
+	# fill in null prediction with hotel cumulative share
+	# ensure that those with non-null predictions are always above those null-filled
+	# using this ghetto formula
+	joined['score'] = np.where(joined['score'].isnull(), 
+		joined['score'].min() - (1 / (joined['hotel_cumulative_share'] + 1)), 
+		joined['score'])
 
 	ndcg_default = lm.ndcg(
 	    joined,
@@ -81,7 +94,10 @@ def __main__():
 	    groupby='search_request_id',
 	    a='score',
 	    b='price_rank')
-	mlflow.log_metric('tau_price', tau_price.mean())
+	tau_price_mean = tau_price.mean()
+	mlflow.log_metric('tau_price', tau_price_mean)
+	print('Tau between model and price is {}'.format(tau_price_mean))
+
 
 	select_hotels = [895, 473, 80698, 69296, 82110, 44, 2403, 52400, 2600, 4046]
 	joined['select_hotels'] = joined['hotel_id'].isin(select_hotels)
@@ -91,8 +107,10 @@ def __main__():
 		ranker='score',
 		label='select_hotels')
 	mlflow.log_metric('mrr_important_hotels', mrr_important_hotels)
+	print('MRR on important hotels is {}'.format(mrr_important_hotels))
 
 	mlflow.end_run()
+	print('Evaluation complete')
 
 if __name__ == '__main__':
 	__main__()
